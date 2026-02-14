f7aa15bd678ff63b9895a0f154a057927166fec0
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-02-14 15:26:56 +0100

    printed actors cards

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Cards from "./assets/css/components/Cards";
 
 export default function App() {
   const [actresses, setActresses] = useState([]);
+  const [actors, setActors] = useState([]);
 
   useEffect(() => {
     axios
@@ -13,11 +14,19 @@ export default function App() {
 
         setActresses(response.data);
       });
+    axios
+      .get("https://lanciweb.github.io/demo/api/actors/")
+      .then((response) => {
+        console.log(response);
+
+        setActors(response.data);
+      });
   }, []);
 
   return (
     <section className="p-5">
       <div className="container">
+        <h1 className="text-white">ATTRICI</h1>
         <div className="row row-cols-3 g-4">
           {actresses.map((actress) => {
             return (
@@ -29,6 +38,24 @@ export default function App() {
                 nationality={actress.nationality}
                 biography={actress.biography}
                 awards={actress.awards}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <div className="container">
+        <h2 className="h1 text-white">ATTORI</h2>
+        <div className="row row-cols-3 g-4">
+          {actors.map((actor) => {
+            return (
+              <Cards
+                key={actor.id}
+                image={actor.image}
+                name={actor.name}
+                birth_year={actor.birth_year}
+                nationality={actor.nationality}
+                biography={actor.biography}
+                awards={actor.awards}
               />
             );
           })}
