9deeaf2793bca6d1e0613d4fed3b9098a4028c8d
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-02-14 13:52:57 +0100

    created card components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Cards from "./assets/css/components/Cards";
 
 export default function App() {
   const [actresses, setActresses] = useState([]);
@@ -23,52 +24,15 @@ export default function App() {
           <div className="row row-cols-3 g-4">
             {actresses.map((actress) => {
               return (
-                <>
-                  <div key={actress.id} className="col">
-                    <div className="card mb-2 h-100 p-3">
-                      <img
-                        src={actress.image}
-                        className="card-img-top h-50 object-fit-cover rounded"
-                        alt={actress.name}
-                      />
-                      <div className="card-body rounded mb-2">
-                        <h5 className="card-title fw-bolder fs-4 mb-3">
-                          {actress.name}
-                        </h5>
-                        <div className="row row-cols-2">
-                          <div className="col-6">
-                            <p className="card-text mb-2">
-                              <span className="d-block fw-bold">Birth:</span>
-                              {actress.birth_year}
-                            </p>
-                          </div>
-                          <div className="col-6">
-                            <p className="card-text mb-2">
-                              <span className="d-block fw-bold">
-                                Nationality:
-                              </span>
-                              {actress.nationality}
-                            </p>
-                          </div>
-                          <div className="col-12">
-                            <p className="card-text mb-2">
-                              <span className="d-block fw-bold">
-                                Biography:
-                              </span>{" "}
-                              {actress.biography}
-                            </p>
-                          </div>
-                          <div className="col-12">
-                            <p className="card-text">
-                              <span className="d-block fw-bold">Awards:</span>{" "}
-                              {actress.awards === "None" ? "-" : actress.awards}
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </>
+                <Cards
+                  key={actress.id}
+                  image={actress.image}
+                  name={actress.name}
+                  birth_year={actress.birth_year}
+                  nationality={actress.nationality}
+                  biography={actress.biography}
+                  awards={actress.awards}
+                />
               );
             })}
           </div>
---
+++ src/assets/css/components/Cards.jsx
@@ -0,0 +1,48 @@
+export default function Cards({
+  image,
+  name,
+  birth_year,
+  nationality,
+  biography,
+  awards,
+}) {
+  return (
+    <div className="col">
+      <div className="card mb-2 h-100 p-3">
+        <img
+          src={image}
+          className="card-img-top h-50 object-fit-cover rounded"
+          alt={name}
+        />
+        <div className="card-body rounded mb-2">
+          <h5 className="card-title fw-bolder fs-4 mb-3">{name}</h5>
+          <div className="row row-cols-2">
+            <div className="col-6">
+              <p className="card-text mb-2">
+                <span className="d-block fw-bold">Birth:</span>
+                {birth_year}
+              </p>
+            </div>
+            <div className="col-6">
+              <p className="card-text mb-2">
+                <span className="d-block fw-bold">Nationality:</span>
+                {nationality}
+              </p>
+            </div>
+            <div className="col-12">
+              <p className="card-text mb-2">
+                <span className="d-block fw-bold">Biography:</span> {biography}
+              </p>
+            </div>
+            <div className="col-12">
+              <p className="card-text">
+                <span className="d-block fw-bold">Awards:</span>{" "}
+                {awards === "None" ? "-" : awards}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
